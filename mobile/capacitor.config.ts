71a1b83a15b697272c53cf0b21fb6d73a8ec6de7
eneokonaniebiet <eneokonaniebiet@gmail.com>
2026-09-22 17:16:43 +0100

    Add Capacitor configuration

---
+++ mobile/capacitor.config.ts
@@ -0,0 +1,3 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+const config: CapacitorConfig = {appId:'com.kemzy.deepcam',appName:'DeepCam Live',webDir:'dist',bundledWebRuntime:false,server:{androidScheme:'https'}}
+export default config
